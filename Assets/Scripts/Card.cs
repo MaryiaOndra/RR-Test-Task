6689670d11405fc48778f2eac8896eed7b48f1fe
Maryia Ondra <ya.ondra@yandex.by>
2021-03-14 00:33:52 +0300

    added card prefab and instantiate cards

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    const string IMAGE_PATH = "https://picsum.photos/200/300";
+
+    [SerializeField] Image icon;
+    [SerializeField] TMP_Text title;
+    [SerializeField] TMP_Text discription;
+    [SerializeField] TMP_Text mana;
+    [SerializeField] TMP_Text attack;
+    [SerializeField] TMP_Text health;
+
+
+    private void Start()
+    {
+        StartCoroutine(SetImageFromWebToCard());
+    }
+
+    IEnumerator SetImageFromWebToCard()
+    {
+        using (var uwr = new UnityWebRequest(IMAGE_PATH, UnityWebRequest.kHttpVerbGET))
+        {
+            uwr.downloadHandler = new DownloadHandlerTexture();
+            yield return uwr.SendWebRequest();
+            Texture2D _texture = DownloadHandlerTexture.GetContent(uwr);
+            Sprite _sprite = Sprite.Create(_texture, new Rect(0.0f, 0.0f, _texture.width, _texture.height), new Vector2(0.5f, 0.5f), 100.0f);
+            icon.sprite = _sprite;
+        }
+    }
+
+    public void SetIcon(Sprite cardSprite) 
+    {
+        icon.sprite = cardSprite;
+    }
+}
---
+++ Assets/Scripts/CardController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CardController : MonoBehaviour
+{
+    [SerializeField] GameObject cardPrefab;
+
+    Card card;
+    GameObject actualCard;
+    GameObject[] cards;
+
+    float radius = 3;
+    float angle;
+
+    float increment = 12;
+
+    void Start()
+    {
+        int numOfCards = Random.Range(4, 7);
+        angle = 90 / numOfCards;
+        FulfilPlayersHand(numOfCards);
+    }
+
+    void FulfilPlayersHand(int numOfCards) 
+    {
+        cards = new GameObject[numOfCards];
+        Vector3 prevPos = new Vector3(-3, 0);
+        Vector3 screenPoint = new Vector3(transform.position.x, transform.position.y - 10);
+
+        for (int i = 0; i < numOfCards; i++)
+        {
+            var actualCard = Instantiate(cardPrefab, Vector3.zero, Quaternion.identity, transform);
+            cards[i] = actualCard;
+
+            //actualCard.transform.RotateAround(screenPoint, Vector3.forward, angle);
+            //angle -= increment;            
+        }
+
+        for (int i = 0; i < cards.Length; i++)
+        {
+            cards[i].transform.position = new Vector3(prevPos.x + 2, cards[i].transform.position.y);
+            cards[i].transform.LookAt(screenPoint, Vector3.zero);
+            prevPos = cards[i].transform.position;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
