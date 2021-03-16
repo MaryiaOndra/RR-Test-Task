01e02694c87066ac4ce2e019432b3cb0c05d23e1
Maryia Ondra <ya.ondra@yandex.by>
2021-03-16 22:25:49 +0300

    finished task

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,192 +1,151 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using TMPro;
 using UnityEngine.UI;
 using DG.Tweening;
 
-enum CardStats 
+namespace RRTestTask
 {
-    Mana,
-    Attack,
-    Health,
-    Lenght
-}
-public class Card : MonoBehaviour
-{
-    const string IMAGE_PATH = "https://picsum.photos/200/300";
-
-    [SerializeField] Image icon;
-    [SerializeField] TMP_Text title;
-    [SerializeField] TMP_Text discription;
-    [SerializeField] TMP_Text text;
-
-    [Header("Stats")]
-    [SerializeField] TMP_Text manaTxt;
-    [SerializeField] TMP_Text attackTxt;
-    [SerializeField] TMP_Text healthTxt;
-
-    public int ManaValue { get; private set; }
-    public int AttackValue { get; private set; }
-    public int HealthValue { get; private set; }
-
-    TMP_Text _tweenTxt;
-
-    private void Start()
+    enum CardStats
     {
-        SetRandomStats();
-        StartCoroutine(SetImageFromWebToCard());
+        Mana,
+        Attack,
+        Health,
+        Lenght
     }
-
-    IEnumerator SetImageFromWebToCard()
+    public class Card : MonoBehaviour
     {
-        using (var uwr = new UnityWebRequest(IMAGE_PATH, UnityWebRequest.kHttpVerbGET))
+        const string IMAGE_PATH = "https://picsum.photos/200/300";
+
+        [SerializeField] Image icon;
+        [SerializeField] TMP_Text title;
+        [SerializeField] TMP_Text discription;
+
+        [Header("Stats")]
+        [SerializeField] TMP_Text manaTxt;
+        [SerializeField] TMP_Text attackTxt;
+        [SerializeField] TMP_Text healthTxt;
+
+        [Header("Tween Stats")]
+        [SerializeField] TMP_Text tweenManaTxt;
+        [SerializeField] TMP_Text tweenAttackTxt;
+        [SerializeField] TMP_Text tweenHealthTxt;
+
+        bool isFirsRandom = true;
+        int statsMaxInt = 10;
+        int statsMinInt = 1;
+
+        public int ManaValue { get; private set; }
+        public int AttackValue { get; private set; }
+        public int HealthValue { get; private set; }
+        public bool IsDead { get; private set; }
+
+
+        private void Start()
         {
-            uwr.downloadHandler = new DownloadHandlerTexture();
-            yield return uwr.SendWebRequest();
-            Texture2D _texture = DownloadHandlerTexture.GetContent(uwr);
-            Sprite _sprite = Sprite.Create(_texture, new Rect(0.0f, 0.0f, _texture.width, _texture.height), new Vector2(0.5f, 0.5f), 100.0f);
-            icon.sprite = _sprite;
-        }
-    }
-
-    void SetRandomStats() 
-    {
-        int _rndValue = Random.Range(1, 10);
-        SetRandomStat(_rndValue, CardStats.Health, healthTxt);
-
-        _rndValue = Random.Range(1, 10);
-        SetRandomStat(_rndValue, CardStats.Mana, manaTxt); 
-        
-        _rndValue = Random.Range(1, 10);
-        SetRandomStat(_rndValue, CardStats.Attack, attackTxt);
-
-        Debug.Log("HealthValue" + HealthValue );
-        Debug.Log("ManaValue" + ManaValue);
-        Debug.Log("AttackValue" + AttackValue);
-    }
-
-    //void SetRandomMana(int rndValue) 
-    //{
-    //    int diff = rndValue - ManaValue;
-
-    //    if (rndValue != ManaValue)
-    //    {
-    //        CreateTextTween(diff, manaTxt);
-    //    }
-
-    //    ManaValue = rndValue;
-    //    manaTxt.text = ManaValue.ToString();       
-    //}
-
-    //void SetRandomAttack(int rndValue)
-    //{
-    //    int diff = rndValue - AttackValue;
-
-    //    if (rndValue != ManaValue)
-    //    {
-    //        CreateTextTween(diff, attackTxt);
-    //    }
-
-    //    AttackValue = rndValue;
-    //    attackTxt.text = AttackValue.ToString();
-    //}
-
-    //void SetRandomHealth(int rndValue)
-    //{
-    //    int diff = rndValue - HealthValue;
-
-    //    if (rndValue != ManaValue)
-    //    {
-    //        CreateTextTween(diff, healthTxt);
-    //    }
-
-    //    HealthValue = rndValue;
-    //    healthTxt.text = HealthValue.ToString();
-    //}
-
-    void SetRandomStat(int rndValue, CardStats cardStats, TMP_Text textToShow) 
-    {
-        int _property = 0;
-
-        switch (cardStats)
-        {
-            case CardStats.Mana:
-                _property = ManaValue;
-                ManaValue = rndValue; 
-                break;
-            case CardStats.Attack:
-                _property = AttackValue;
-                AttackValue = rndValue;
-                break;
-            case CardStats.Health:
-                _property = HealthValue;
-                HealthValue = rndValue;
-                break;
-        }
-       
-        int _diff = rndValue - _property;
-
-        if (rndValue != _property)
-        {
-            CreateTextTween(_diff, textToShow);
+            SetRandomStats();
+            isFirsRandom = false;
+            StartCoroutine(SetImageFromWebToCard());
         }
 
-        _property = rndValue;
-        textToShow.text = _property.ToString();
-    }
-
-    private void CreateTextTween(int diff ,TMP_Text txt)
-    {
-        _tweenTxt = Instantiate(txt, transform);
-        float _duration = 2f;
-        float _scale = 2f;
-        float _alfa = 0f;
-
-        if (diff < 0)
+        IEnumerator SetImageFromWebToCard()
         {
-            _tweenTxt.text = diff.ToString();
-            _tweenTxt.color = Color.red;
-            Debug.Log("MANA" + _tweenTxt.text);
-        }
-        else
-        {
-            _tweenTxt.text = "+" + diff;
-            _tweenTxt.color = Color.blue;
+            Vector2 _pivotPoint = new Vector2(0.5f, 0.5f);
+            float _pixelsPerUnit = 100f;
+
+            using (var uwr = new UnityWebRequest(IMAGE_PATH, UnityWebRequest.kHttpVerbGET))
+            {
+                uwr.downloadHandler = new DownloadHandlerTexture();
+                yield return uwr.SendWebRequest();
+                Texture2D _texture = DownloadHandlerTexture.GetContent(uwr);
+                Rect _rect = new Rect(0.0f, 0.0f, _texture.width, _texture.height);
+                Sprite _sprite = Sprite.Create(_texture, _rect, _pivotPoint, _pixelsPerUnit);
+                icon.sprite = _sprite;
+            }
         }
 
-        _tweenTxt.transform.DOScale(_scale, _duration);
-        _tweenTxt.DOFade(_alfa, _duration).OnComplete(DestroyText);
-    }
-
-    void DestroyText() 
-    {
-        Destroy(_tweenTxt);
-    }
-
-
-    public void SetIcon(Sprite cardSprite) 
-    {
-        icon.sprite = cardSprite;
-    }
-
-    public void ChangeRandomValue() 
-    {
-        int _rndIndx = Random.Range(0, (int)CardStats.Lenght);
-        int _rndValue = Random.Range(-2, 10);
-
-        switch (_rndIndx)
+        void SetRandomStats()
         {
-            case (int)CardStats.Mana:
-                SetRandomStat(_rndValue, CardStats.Mana, manaTxt);
-                break;
-            case (int)CardStats.Attack:
-                SetRandomStat(_rndValue, CardStats.Attack, attackTxt);
-                break;
-            case (int)CardStats.Health:
-                SetRandomStat(_rndValue, CardStats.Health, healthTxt);
-                break;
+            int _rndValue = Random.Range(statsMinInt, statsMaxInt);
+            SetStat(_rndValue, CardStats.Health, healthTxt, tweenHealthTxt);
+
+            _rndValue = Random.Range(statsMinInt, statsMaxInt);
+            SetStat(_rndValue, CardStats.Mana, manaTxt, tweenManaTxt);
+
+            _rndValue = Random.Range(statsMinInt, statsMaxInt);
+            SetStat(_rndValue, CardStats.Attack, attackTxt, tweenAttackTxt);
+        }
+
+        void SetStat(int rndValue, CardStats cardStats, TMP_Text textToShow, TMP_Text textToTween)
+        {
+            int _property = 0;
+
+            switch (cardStats)
+            {
+                case CardStats.Mana:
+                    _property = ManaValue;
+                    ManaValue = rndValue;
+                    break;
+                case CardStats.Attack:
+                    _property = AttackValue;
+                    AttackValue = rndValue;
+                    break;
+                case CardStats.Health:
+                    _property = HealthValue;
+                    HealthValue = rndValue;
+                    if (HealthValue < 0)
+                        IsDead = true;
+                    break;
+            }
+
+            int _diff = rndValue - _property;
+
+            if (!isFirsRandom && rndValue != _property)
+            {
+                CreateTextTween(_diff, textToTween);
+            }
+
+            _property = rndValue;
+            textToShow.text = _property.ToString();
+        }
+
+        private void CreateTextTween(int diff, TMP_Text tweenTxt)
+        {
+            float _duration = 2.5f;
+            float _alfa = 0f;
+
+            if (diff < 0)
+            {
+                tweenTxt.text = diff.ToString();
+                tweenTxt.color = Color.red;
+            }
+            else
+            {
+                tweenTxt.text = "+" + diff;
+                tweenTxt.color = Color.green;
+            }
+
+            tweenTxt.DOFade(_alfa, _duration);
+        }
+
+        public void ChangeRandomValue()
+        {
+            int _rndIndx = Random.Range(0, (int)CardStats.Lenght);
+            int _rndValue = Random.Range(-2, 10);
+
+            switch (_rndIndx)
+            {
+                case (int)CardStats.Mana:
+                    SetStat(_rndValue, CardStats.Mana, manaTxt, tweenManaTxt);
+                    break;
+                case (int)CardStats.Attack:
+                    SetStat(_rndValue, CardStats.Attack, attackTxt, tweenAttackTxt);
+                    break;
+                case (int)CardStats.Health:
+                    SetStat(_rndValue, CardStats.Health, healthTxt, tweenHealthTxt);
+                    break;
+            }
         }
     }
 }
--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -1,153 +1,161 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.Networking;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using System.Collections.Generic;
 
-public class CardController : MonoBehaviour
+namespace RRTestTask
 {
-    [SerializeField] GameObject cardPrefab;
-    [SerializeField] float offsetRotation;
-    [SerializeField] float offsetTarget;
-    [SerializeField] Button randomizeBtn;
-    [SerializeField] Transform usedCardsPoint;
-    [SerializeField] Transform actualDeckPont;
-
-    List<GameObject> cards = new List<GameObject>();
-
-    int cardNum = 0;
-    int numOfCards;
-    Transform _prevCardTr;
-
-    void Start()
+    public class CardController : MonoBehaviour
     {
-        numOfCards = Random.Range(4, 7);
-        FulfilPlayersHand(numOfCards);
-        SetCardsPosition(cards);
-        RotateAllCards(cards);
-        randomizeBtn.onClick.AddListener(ChangeRandomCardValue);
-    }
+        [SerializeField] GameObject cardPrefab;
+        [SerializeField] Button randomizeBtn;
+        [SerializeField] Transform usedCardsPoint;
+        [SerializeField] Transform actualDeckPont;
 
-    void FulfilPlayersHand(int numOfCards) 
-    {
-        for (int i = 0; i < numOfCards; i++)
+        List<GameObject> cards = new List<GameObject>();
+        Transform removedCardTr;
+        Transform newCartTr;
+
+        float offceеExtremeCards = 3.5f;
+        float offsetRotation = 90f;
+        float offsetTarget = -20f;
+        int cardIndx = 0;
+        int numOfCards;
+        int minCardNum = 4;
+        int maxCardNum = 7;
+        
+        void Start()
         {
-            var actualCard = Instantiate(cardPrefab, Vector3.zero, Quaternion.identity, transform);
-            cards.Add(actualCard);    
+            numOfCards = Random.Range(minCardNum, maxCardNum);
+            FulfilPlayersHand(numOfCards);
+            SetCardsPosition(cards);
+            randomizeBtn.onClick.AddListener(ChangeRandomCardValue);
         }
-    }
 
-    void SetCardsPosition(List<GameObject> cards) 
-    {
-        Vector3 _startPos = new Vector3(-3, -3, 0);
-        Vector3 _endPos = new Vector3(3, -3, 0);
-
-        for (int i = 0; i < cards.Count; i++)
+        void FulfilPlayersHand(int numOfCards)
         {
-            float _t = 1f / numOfCards * i;
-            cards[i].transform.position = Vector3.Lerp(_startPos, _endPos, _t);
-        }
-    }
-
-    void RotateAllCards(List<GameObject> cardsToRot) 
-    {
-       Vector3 _targetPos = FindRotateTarget();
-
-        for (int i = 0; i < cardsToRot.Count; i++)
-        {
-           RotateCard(cardsToRot[i], _targetPos);
-        }
-    }
-
-    Vector3 FindRotateTarget() 
-    {
-       Vector3 _startPos = cards[0].transform.position;
-       Vector3 _finalPos = cards[cards.Count - 1].transform.position;
-       float _middlePosX = (_startPos.x + _finalPos.x) * 0.5f;
-       Vector3 _targetPos = new Vector3(_middlePosX,  offsetTarget);
-       return _targetPos;
-    }
-
-    void RotateCard(GameObject cardToRotate, Vector3 targetPos)
-    {
-        Vector3 _targetPos = targetPos;
-        Vector3 _thisPos = cardToRotate.transform.position;
-        _targetPos.x -= _thisPos.x;
-        _targetPos.y -= _thisPos.y;
-
-        float _angle = Mathf.Atan2(_targetPos.y, _targetPos.x) * Mathf.Rad2Deg;
-        cardToRotate.transform.rotation = Quaternion.Euler(new Vector3(0, 0, _angle + offsetRotation));
-        float _maxAngle = Mathf.Abs(cards[0].transform.rotation.normalized.z);
-        float _minAngle = 0f;
-        float _insideAngle = Mathf.Abs(cardToRotate.transform.rotation.normalized.z);
-        float _t = (_maxAngle - _insideAngle) / 
-           (_maxAngle - _minAngle);
-        float _yLocalPos = Mathf.Lerp(0, 1, _t);
-        float _posYOffcet = numOfCards * 0.1f - 0.1f;
-        float _newYPos = _yLocalPos * _posYOffcet;
-
-        cardToRotate.transform.position = new Vector3(cardToRotate.transform.position.x, _newYPos);
-    }
-
-    void ChangeRandomCardValue() 
-    {
-        cards[cardNum++].GetComponent<Card>().ChangeRandomValue();
-
-        if (cardNum >= cards.Count)
-            cardNum = 0;
-
-        CheckCardsHealth();
-    }
-
-    void CheckCardsHealth()
-    {
-        int _removedCardIdnx;
-        GameObject _removedCard;
-        float _moveDuration = 0.5f;
-
-        for (int i = 0; i < cards.Count; i++)
-        {
-            int cardHealth = cards[i].GetComponent<Card>().HealthValue;
-
-            if (cardHealth <= 0)
+            for (int i = 0; i < numOfCards; i++)
             {
-                _removedCardIdnx = i;
-                _removedCard = cards[i];
-                cards.Remove(cards[i]);
-
-                _removedCard.transform.DOMove(usedCardsPoint.position, _moveDuration);
-                _removedCard.transform.DORotate(Vector3.zero, _moveDuration);
-
-                RepositeOtherCards(_removedCardIdnx, _removedCard);
-                CreateNewCardInstead(_prevCardTr);
+                var actualCard = Instantiate(cardPrefab, Vector3.zero, Quaternion.identity, transform);
+                cards.Add(actualCard);
             }
-        }       
-    }
-
-    void RepositeOtherCards(int cardIndx, GameObject cardTr)
-    {
-        _prevCardTr = cardTr.transform;
-        Transform _actualCardTr;
-        float _moveDuration = 1f;
-
-        for (int i = cardIndx; i < cards.Count; i++)
-        {
-            _actualCardTr = cards[i].transform;
-            cards[i].transform.DOMove(_prevCardTr.position, _moveDuration);
-            cards[i].transform.DORotateQuaternion(_prevCardTr.rotation, _moveDuration);
-            _prevCardTr = _actualCardTr;
         }
-    }
 
-    void CreateNewCardInstead(Transform prevCardTr) 
-    {
-        Debug.LogWarning("Creating NEW CARD");
-        float _moveDuration = 1.5f;
-        GameObject newCard = Instantiate(cardPrefab, actualDeckPont.position, Quaternion.identity, actualDeckPont);
-        newCard.transform.DOMove(prevCardTr.position, _moveDuration);
-        newCard.transform.DOLocalRotateQuaternion(prevCardTr.rotation, _moveDuration);
-        cards.Add(newCard);
+        #region SetCardsPosition
+
+        void SetCardsPosition(List<GameObject> cards)
+        {
+            Vector3 _startPos = new Vector3(-offceеExtremeCards, 0, 0);
+            Vector3 _endPos = new Vector3(offceеExtremeCards, 0, 0);
+
+            for (int i = 0; i < cards.Count; i++)
+            {
+                float _t = 1f / numOfCards * i;
+                cards[i].transform.position = Vector3.Lerp(_startPos, _endPos, _t);
+            }
+
+            RotateAllCards(cards);
+        }
+
+        void RotateAllCards(List<GameObject> cardsToRot)
+        {
+            Vector3 _targetPos = FindRotateTarget();
+
+            for (int i = 0; i < cardsToRot.Count; i++)
+            {
+                RotateCard(cardsToRot[i], _targetPos);
+            }
+        }
+
+        Vector3 FindRotateTarget()
+        {
+            Vector3 _startPos = cards[0].transform.position;
+            Vector3 _finalPos = cards[cards.Count - 1].transform.position;
+            float _middlePosX = (_startPos.x + _finalPos.x) * 0.5f;
+            Vector3 _targetPos = new Vector3(_middlePosX, offsetTarget);
+            return _targetPos;
+        }
+
+        void RotateCard(GameObject cardToRotate, Vector3 targetPos)
+        {
+            Vector3 _targetPos = targetPos;
+            Vector3 _thisPos = cardToRotate.transform.position;
+            float _posYOffcet = 0.1f;
+            float _minAngle = 0.02f;
+
+            _targetPos.x -= _thisPos.x;
+            _targetPos.y -= _thisPos.y;
+
+            float _angle = Mathf.Atan2(_targetPos.y, _targetPos.x) * Mathf.Rad2Deg;
+            cardToRotate.transform.rotation = Quaternion.Euler(new Vector3(0, 0, _angle + offsetRotation));
+            float _maxAngle = Mathf.Abs(cards[0].transform.rotation.normalized.z);
+            float _insideAngle = Mathf.Abs(cardToRotate.transform.rotation.normalized.z);
+            float _t = (_maxAngle - _insideAngle) / (_maxAngle - _minAngle);
+            float _yLocalPos = Mathf.Lerp(0, 1, _t);
+            _posYOffcet *= numOfCards;
+            float _newYPos = _yLocalPos * _posYOffcet;
+
+            cardToRotate.transform.position = new Vector3(cardToRotate.transform.position.x, _newYPos);
+        }
+
+        #endregion
+
+        #region ChangeRandomCardValue
+        void ChangeRandomCardValue()
+        {
+            if (cardIndx >= cards.Count)
+                cardIndx = 0;
+
+            Card _card = cards[cardIndx].GetComponent<Card>();
+            _card.ChangeRandomValue();
+
+            if (_card.HealthValue <= 0)
+            {
+                RemoveCardFromHand(cardIndx);
+                RepositeOtherCards(removedCardTr);
+                CreateNewCardInstead();
+            }
+
+            cardIndx++;
+        }
+
+        private void RemoveCardFromHand(int cardIndx)
+        {
+            float _moveDuration = 2f;
+            
+            removedCardTr = cards[cardIndx].transform;
+            cards[cardIndx].transform.DOMove(usedCardsPoint.position, _moveDuration);
+            cards[cardIndx].transform.DORotate(Vector3.zero, _moveDuration);
+            cards.Remove(cards[cardIndx]);
+        }
+
+        private void RepositeOtherCards(Transform removedCard)
+        {
+            Transform _prevCardTransf = removedCard;
+            Transform _actualCardTr;
+            float _moveDuration = 1f;
+
+            for (int i = cardIndx; i < cards.Count; i++)
+            {
+                _actualCardTr = cards[i].transform;
+                cards[i].transform.DOMove(_prevCardTransf.position, _moveDuration);
+                cards[i].transform.DORotateQuaternion(_prevCardTransf.rotation, _moveDuration);
+                _prevCardTransf = _actualCardTr;
+            }
+
+            newCartTr = _prevCardTransf;
+        }
+
+        private void CreateNewCardInstead()
+        {
+            float _moveDuration = 2f;
+
+            var actualCard = Instantiate(cardPrefab, actualDeckPont.position, Quaternion.identity, transform);
+            actualCard.transform.DOMove(newCartTr.position, _moveDuration);
+            actualCard.transform.DORotateQuaternion(newCartTr.rotation, _moveDuration);
+            cards.Add(actualCard);
+        }
+
+        #endregion
     }
-    
 }
